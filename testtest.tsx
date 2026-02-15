39832b65799ff700bf611e4090a962377e6f8259
AndyMuande <156927973+AndyMuande@users.noreply.github.com>
2026-02-15 13:57:16 +0100

    Création du composant testtest.tsx avec un composant simple

---
+++ testtest.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface TestComponentProps {
+  title?: string;
+}
+
+const TestComponent: React.FC<TestComponentProps> = ({ title = "Test Component" }) => {
+  return (
+    <div className="test-component">
+      <h1>{title}</h1>
+      <p>This is a simple test component.</p>
+      <button onClick={() => console.log('Button clicked!')}>
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default TestComponent;
