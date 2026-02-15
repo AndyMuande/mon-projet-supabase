6ec50aa5502e2d5eac9d5cc5006d32e662f4ef23
AndyMuande <156927973+AndyMuande@users.noreply.github.com>
2026-02-15 13:02:54 +0100

    =Création du composant testtest.tsx

---
+++ =components/testtest.tsx
@@ -0,0 +1,12 @@
+=import React from 'react';
+
+const TestTest: React.FC = () => {
+  return (
+    <div className="test-test-component">
+      <h1>Composant de Test</h1>
+      <p>Ceci est un composant de test très simple.</p>
+    </div>
+  );
+};
+
+export default TestTest;
