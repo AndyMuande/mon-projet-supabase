ec41879005fdd39387154eae8352ba4c562f53d8
AndyMuande <156927973+AndyMuande@users.noreply.github.com>
2026-02-16 13:45:05 +0100

    Création de la page de détails des livres

---
+++ app/books/details/page.tsx
@@ -0,0 +1,16 @@
+
+'use client';
+
+import React from 'react';
+
+const PageDetailsBooks = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Détails du Livre</h1>
+      <p>Ceci est la page de détails pour un livre.</p>
+      {/* Ici, vous afficherez les détails spécifiques du livre */}
+    </div>
+  );
+};
+
+export default PageDetailsBooks;
